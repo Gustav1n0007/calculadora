9ca274adf01ccec14fa319445dc4859f55a6b4ed
gustavo rodrigues <gustavolourenco821@gmail.com>
2024-09-17 11:22:56 -0300

    add: tela de carregamento troca de tela

--- frmSplash.cs
+++ frmSplash.cs
@@ -2,6 +2,7 @@ namespace calculadora
 {
     public partial class frmSplash : Form
     {
+        Random random = new Random();
         public frmSplash()
         {
             InitializeComponent();
@@ -10,6 +11,21 @@ namespace calculadora
         private void frmSplash_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void tmrLoading_Tick(object sender, EventArgs e)
+        {
+            if (carregamentoFalso.Value < 100)
+            {
+                carregamentoFalso.Value = carregamentoFalso.Value + 10;
+            }
+            if (carregamentoFalso.Value == 100)
+            {
+                calculadora frmTeste = new calculadora();
+                frmTeste.Show();
+                tmrLoading.Stop();
+                this.Hide();
+            }
         }
     }
 }
